571b487f24c71f737c5d2f2d9d38df209d983b25
Daniyarkadyrbekov <d.kadyrbekov@mail.ru>
2017-05-17 23:26:47 +0300

    Fixed nod js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-/**
- * Created by daniarkadyrbekov on 16.05.17.
- */
 'use strict';
 
 const expr = require("express");
@@ -8,6 +5,7 @@ const expr = require("express");
 const app = expr();
 
 app.use(expr.static('./'));
+
 
 app.listen(process.env.PORT || 3000, '0.0.0.0', function () {
     console.log(`Example app listening on port ${process.env.PORT || 3000} !`);
